179c122eac6ad3e6ab3d3e31e22cf4307bbaf98e
Matthew Farrell <farrellm@alum.mit.edu>
2014-11-02 10:17:34 -0500

    initial

---
+++ project.clj
@@ -0,0 +1,10 @@
+(defproject carbonite "0.1.0-SNAPSHOT"
+  :description "Serialize the unserializable"
+  :url "https://github.com/farrellm/carbonite"
+  :license {:name "Apache License"
+            :url "http://www.apache.org/licenses/"}
+  :dependencies [[openjdk/tools "7"]]
+  :java-source-paths ["src-java"]
+
+  :manifest {"Agent-Class" "carbonite.BytecodeVault"
+             "Can-Retransform-Classes" "true"})
